ccf2ed578919da325a7a3726e68b8c18fe9ed7b5
baptistejouin <baptiste.jouin44@gmail.com>
2022-02-07 21:58:53 +0100

    Setup the project.

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+	<HelloWorldVue msg="Hello with Vite"></HelloWorldVue>
+</template>
+
+<script>
+
+import HelloWorldVue from "@/components/HelloWorld.vue"
+
+export default {
+	name: 'Home',
+	components : {
+		HelloWorldVue
+	}
+}
+</script>
